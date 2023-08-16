d0a1d4e8077c1791a0594acea5eb52fe9198809d
AshuMokale <ashishmokale2002@gmail.com>
2023-08-16 20:41:09 +0530

    Created and wrote main game file.

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Main class to manage it all."""
+    def __init__(self):
+        """Initialize the game, and start the window."""
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Capture mouse and keyboard events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # display game window
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    """Create a game instance"""
+    ai = AlienInvasion()
+    ai.run_game()
