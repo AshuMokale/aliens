2099adc45728cae81af0d690fa49302aac4e4e9b
Ashish Mokale <62004342+AshuMokale@users.noreply.github.com>
2023-08-18 13:09:17 +0530

    Added code for 60fps in the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ class AlienInvasion:
         """Initialize the game, and start the window."""
 
         pygame.init()
+        self.clock = pygame.time.Tick()
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
@@ -21,6 +22,7 @@ class AlienInvasion:
 
             # display game window
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     """Create a game instance"""
